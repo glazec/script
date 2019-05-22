9cec816b40c3e6e5f088ed4bd134b64c7f2c34f5
glaze <i@inevitable.tech>
2019-05-22 16:24:11 -0700

    word_recite: add intro

--- word_recite.py
+++ word_recite.py
@@ -1,66 +1,67 @@
+# This script is used to recite something. Keep reciting the words unitil you get everything right
 import random
-dict={'appreciate':'gerunds',
-'avoid':'gerunds',
-'anticipate':'gerunds',
-'complete':'gerunds',
-'consider':'gerunds',
-'delay':'gerunds',
-'deny':'gerunds',
-'discuss':'gerunds',
-'dislike':'gerunds',
-'enjoy':'gerunds',
-'finish':'gerunds',
-'keep':'gerunds',
-'involve':'gerunds',
-'mention':'gerunds',
-'mind':'gerunds',
-'miss':'gerunds',
-'practice':'gerunds',
-'quit':'gerunds',
-'recommend':'gerunds',
-'suggest':'gerunds',
-'tolerate':'gerunds',
+dict = {'appreciate': 'gerunds',
+        'avoid': 'gerunds',
+        'anticipate': 'gerunds',
+        'complete': 'gerunds',
+        'consider': 'gerunds',
+        'delay': 'gerunds',
+        'deny': 'gerunds',
+        'discuss': 'gerunds',
+        'dislike': 'gerunds',
+        'enjoy': 'gerunds',
+        'finish': 'gerunds',
+        'keep': 'gerunds',
+        'involve': 'gerunds',
+        'mention': 'gerunds',
+        'mind': 'gerunds',
+        'miss': 'gerunds',
+        'practice': 'gerunds',
+        'quit': 'gerunds',
+        'recommend': 'gerunds',
+        'suggest': 'gerunds',
+        'tolerate': 'gerunds',
 
-'appologize':'appologize for',
-'plan':'plan on',
-'put':'put off',
+        'appologize': 'appologize for',
+        'plan': 'plan on',
+        'put': 'put off',
 
-'agree':'infinite',
-'afford':'infinite',
-'ask':'infinite',
-'decide':'infinite',
-'demand':'infinite',
-'expect':'infinite',
-'have':'infinite',
-'hope':'infinite',
-'intend':'infinite',
-'invite':'infinite',
-'learn':'infinite',
-'manage':'infinite',
-'need':'infinite',
-'offer':'infinite',
-'order':'infinite',
-'plan':'infinite',
-'pretend':'infinite',
-'remind':'infinite',
-'seem':'infinite',
-'tell':'infinite',
-'want':'infinite',
-'warn':'infinite'}
-correct=0
-incorrect=0
+        'agree': 'infinite',
+        'afford': 'infinite',
+        'ask': 'infinite',
+        'decide': 'infinite',
+        'demand': 'infinite',
+        'expect': 'infinite',
+        'have': 'infinite',
+        'hope': 'infinite',
+        'intend': 'infinite',
+        'invite': 'infinite',
+        'learn': 'infinite',
+        'manage': 'infinite',
+        'need': 'infinite',
+        'offer': 'infinite',
+        'order': 'infinite',
+        'plan': 'infinite',
+        'pretend': 'infinite',
+        'remind': 'infinite',
+        'seem': 'infinite',
+        'tell': 'infinite',
+        'want': 'infinite',
+        'warn': 'infinite'}
+correct = 0
+incorrect = 0
 while dict:
-    a=list(dict.keys())[random.randrange(len(list(dict.keys())))]
+    a = list(dict.keys())[random.randrange(len(list(dict.keys())))]
     print(a)
-    b=input("write the answer:\n")
-    c=dict.get(a)
+    b = input("write the answer:\n")
+    c = dict.get(a)
     if b in c:
         print("correct\n")
-        correct+=1
+        correct += 1
         dict.pop(a)
         print(str(len(list(dict.keys())))+" left")
     else:
-        print(a,"+",c)
-        incorrect+=1
-rate=incorrect/(correct+incorrect)
-print("finish\nscore: ",rate)
+        print(a, "+", c)
+        incorrect += 1
+rate = incorrect/(correct+incorrect)
+print("finish\nscore: ", rate)
